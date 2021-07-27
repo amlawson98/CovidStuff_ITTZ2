681fb4433d031c299919e53ee3be33e1e041e5c7
Ivor-Yidong-Chen <52457461+Ivor-Yidong-Chen@users.noreply.github.com>
2021-07-27 18:08:51 -0500

    Add files via upload

--- Ivor_Folder/test/app/routes.py
+++ Ivor_Folder/test/app/routes.py
@@ -17,7 +17,7 @@ def search():
 
 # Test case: Deleted from database: covid19 table: State row: illinois 2020-01-31
 # Once the add function is written, we should re-insert this row of data
-# data="2020-01-31" state="illinois" fips=17 cases=2 deaths=0
+# date="2020-01-31" state="illinois" fips=17 cases=2 deaths=0
 @app.route("/delete",methods = ['GET','POST'])
 def delete():
     if request.method == 'POST':
@@ -30,7 +30,56 @@ def delete():
     else:
         return redirect('/')
 
+@app.route('/update_case',methods=['GET','POST'])
+def update_case():
+    if request.method == 'POST':
+        state = request.form['state']
+        date = request.form['date']
+        case = request.form['case']
+        case = int(case)
+        conn = db.connect()
+        query = "UPDATE State SET cases={} where state=\"{}\" and date=\"{}\";".format(case,state,date)
+        results = conn.execute(query)
+        return render_template("index.html",date=results)
+    else:
+        return redirect('/')
 
+@app.route('/update_death',methods=['GET','POST'])
+def update_death():
+    if request.method == 'POST':
+        state = request.form['state']
+        date = request.form['date']
+        death = request.form['death']
+        death = int(death)
+        conn = db.connect()
+        query = "UPDATE State SET deaths={} where state=\"{}\" and date=\"{}\";".format(death,state,date)
+        results = conn.execute(query)
+        return render_template("index.html",date=results)
+    else:
+        return redirect('/')
+
+@app.route('/group_by',methods=['GET','POST'])
+def group_by():
+    if request.method == 'POST':
+        date = request.form['date']
+        conn = db.connect()
+        query = "SELECT date, avg(cases) from State group by date having date = \"{}\";".format(date)
+        results = conn.execute(query)
+        return render_template("index.html",data2=results)
+    else:
+        return redirect('/')
+
+@app.route('/max_county',methods=['GET','POST'])
+def max_county():
+    if request.method == 'POST':
+        date = request.form['date']
+        state = request.form['state']
+        conn = db.connect()
+        query = "select County.county, County.state, County.cases from (select date,state, avg(cases) as avg_cases from County group by date, state) as temp, County where temp.date = County.date and temp.state=County.state and County.cases >= temp.avg_cases and temp.date=\"{}\" and temp.state=\"{}\" order by County.state, County.cases desc, County.county;".format(date,state)
+        results = conn.execute(query)
+        return render_template("index.html",data3=results)
+    else:
+        return redirect('/')
 
 
 
