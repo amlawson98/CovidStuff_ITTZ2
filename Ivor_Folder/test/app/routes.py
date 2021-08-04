17366ab5c2d096ac184d0e0c5a35497db3f751eb
Ivor-Yidong-Chen <52457461+Ivor-Yidong-Chen@users.noreply.github.com>
2021-08-04 16:21:10 -0500

    Add files via upload

--- Ivor_Folder/test/app/routes.py
+++ Ivor_Folder/test/app/routes.py
@@ -15,42 +15,6 @@ import datetime
 # from app import database as db_helper
 from app import db
 
-# @app.route('/vaccine',methods = ["GET","POST"])
-# def vaccine():
-#     if request.method == 'POST':
-#         user_input = request.form['state'].lower()
-#         query = "SELECT DISTINCT date, cases, deaths from State where state = \"{}\";".format(user_input)
-#         query2 = "SELECT DISTINCT Vaccine.Date, Vaccine.Doses_Administrated, Vaccine.Stage_2_Dose from Vaccine, State where State.state=\"{}\" and State.fips = Vaccine.State_FIPS and Vaccine.Vaccine_Type = 'ALL';".format(user_input)
-#         conn = db.connect()
-#         results1, results2 = conn.execute(query).fetchall(), conn.execute(query2).fetchall()
-#         date, case, death = [item[0] for item in results1],[item[1] for item in results1], [item[2] for item in results1]
-#         case_diff = [case[i]-case[i-1] for i in range(1,len(case))]
-#         vac_date, dose, stage2 = [item[0] for item in results2], [item[1] for item in results2], [item[2] for item in results2]
-#         dose_diff = [dose[i]-dose[i-1] for i in range(1,len(dose))]
-#         ind = date.index(vac_date[0])
-#         date = date[ind:]
-#         case = case[ind:]
-#         print(date[-1])
-#         print(vac_date[-1])
-#         print(len(vac_date))
-#         print(len(date))
-#         print(len(dose_diff))
-#         print(len(case_diff))
-#         print(case)
-#         with plt.style.context('ggplot'):
-#                 fig,ax = plt.subplots(1,1,figsize=(5,10))
-#                 fig.autofmt_xdate()
-#                 ax.plot(date, case, "-", linewidth = 1, color = 'r', label = "Case Number")
-#                 ax.plot(vac_date, dose, "-", linewidth = 1, color = 'g', label = 'Stage 2 Vaccine')
-#                 ax.legend()
-#         pngImage = io.BytesIO()
-#         Canvas(fig).print_png(pngImage)
-#         pngImageB64String = "data:image/png;base64,"
-#         pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
-#         return render_template("index.html",image = pngImageB64String)
-#     else:
-#         return redirect('/')
-
 @app.route('/wear_mask',methods = ['GET','POST'])
 def wear_mask():
     if request.method == 'POST':
@@ -68,11 +32,14 @@ def wear_mask():
             mask.pop(j-count)
             death.pop(j-count)
             count += 1
-        query1,query2,query3 = 'set @name = \"{}\";'.format(user_input),'call DeathRate(@name,@out);','select @out;'
+        query1,query2,query3,query4,query5,query6 = 'set @name = \"{}\";'.format(user_input),'call DeathRate(@name,@death_corr,@vac_corr, @case_corr,@mask_vac);','select @death_corr;', 'select @vac_corr;', 'select @case_corr;','select @mask_vac;'
         conn.execute(query1)
         conn.execute(query2)
         corr_death = conn.execute(query3).fetchall()
-        query = "select distinct County.county as county_name, Mask.never+Mask.rarely as noMask, temp.final_death_count from County, Mask, (select fips, max(deaths) as final_death_count from County group by fips) as temp where County.state = \"{}\" and Mask.countyfips = County.fips and County.fips = temp.fips;".format(user_input)
+        corr_vac = conn.execute(query4).fetchall()
+        corr_case = conn.execute(query5).fetchall()
+        corr_mask_vac = conn.execute(query6).fetchall()
+        query = "select distinct County.county as county_name, Mask.never+Mask.rarely as noMask, temp.final_death_count from County, Mask, (select fips, max(cases) as final_death_count from County group by fips) as temp where County.state = \"{}\" and Mask.countyfips = County.fips and County.fips = temp.fips;".format(user_input)
         conn = db.connect()
         results = conn.execute(query).fetchall()
         mask, case = [item[1] for item in results], [item[2] for item in results]
@@ -85,12 +52,11 @@ def wear_mask():
             mask.pop(j-count)
             case.pop(j-count)
             count += 1
-        query1,query2,query3 = 'set @name = \"{}\";'.format(user_input),'call CaseRate(@name,@out);','select @out;'
-        conn.execute(query1)
-        conn.execute(query2)
-        corr_case = conn.execute(query3).fetchall()
+        query = "select distinct State.state as state_, Vac.dose as dose, Mask_State.outcome as withMask from (select County.state as state, avg(Mask.frequently)+avg(Mask.always) as outcome from Mask, County where Mask.countyfips = County.fips group by County.state) as Mask_State, (select State_FIPS, max(Doses_Administrated) as dose from Vaccine group by State_FIPS) as Vac, State where State.fips = Vac.State_FIPS and State.state = Mask_State.state;"
+        results = conn.execute(query).fetchall()
+        vac, with_mask = [item[1] for item in results],[item[2] for item in results]
         with plt.style.context('ggplot'):
-                fig,ax = plt.subplots(2,1,figsize=(5,10))
+                fig,ax = plt.subplots(3,1,figsize=(5,20))
                 fig.autofmt_xdate()
                 ax[0].scatter(mask,death)
                 ax[0].set_xlabel('Percentage of Non-Mask Wearer')
@@ -100,6 +66,10 @@ def wear_mask():
                 ax[1].set_xlabel('Percentage of Non-Mask Wearer')
                 ax[1].set_ylabel('Case')
                 ax[1].set_title('Correlation: {}'.format(corr_case[0][0]))
+                ax[2].scatter(with_mask, vac)
+                ax[2].set_xlabel('Percentage of Mask Wearer')
+                ax[2].set_ylabel('Vaccination Count')
+                ax[2].set_title('Correlation: {} Across US'.format(corr_mask_vac[0][0]))
         pngImage = io.BytesIO()
         Canvas(fig).print_png(pngImage)
         pngImageB64String = "data:image/png;base64,"
@@ -335,13 +305,7 @@ def update_death():
     else:
         return redirect('/')
 
-<<<<<<< HEAD
-
-
-@app.route('/group_by', methods=['GET','POST'])
-=======
 @app.route('/group_by',methods=['GET','POST'])
->>>>>>> 22afacb836a1a014352ccae12456197dc0887460
 def group_by():
     if request.method == 'POST':
         date = request.form['date']
